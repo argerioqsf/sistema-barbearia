8e1c6d586c4010dcd1ea75cb71e5ee4b60da3893
Argerio Queiroz <argerioaf3@gmail.com>
2024-02-04 00:16:47 -0300

    ajuste de imports

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,5 @@
+import ContainerSection from "./ContainerSection";
+import FormFieldCheckBox from "./FormFieldCheckBox";
+import FormFieldText from "./FormFieldText";
+
+export { ContainerSection, FormFieldCheckBox, FormFieldText };
