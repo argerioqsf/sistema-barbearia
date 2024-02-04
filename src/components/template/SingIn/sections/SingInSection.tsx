c405ed23608e924c6f9f800f53184709143085b5
Argerio Queiroz <argerioaf3@gmail.com>
2024-02-04 00:21:17 -0300

    ajuste responsividade

--- src/components/template/SingIn/sections/SingInSection.tsx
+++ src/components/template/SingIn/sections/SingInSection.tsx
@@ -4,7 +4,7 @@ import ContainerSection from "@/components/molecules/ContainerSection";
 const SingInSection = () => {
   return (
     <ContainerSection>
-      <div className="w-full flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="w-full flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
         <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-4">
           <FormLogin />
         </div>
