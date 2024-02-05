6b62f18be26a75f6db0f43e86dd0c84a20359e02
Argerio Queiroz <argerioaf3@gmail.com>
2024-02-05 01:07:33 -0300

    ajustes de configuração do locale

--- src/components/template/Home/index.tsx
+++ src/components/template/Home/index.tsx
@@ -1,11 +1,11 @@
 import { usePathTranslations } from "@/hooks/use-path-translations";
 
 const Home = () => {
-  const { te } = usePathTranslations("home.home_section");
+  const { any } = usePathTranslations("metadata.dashboard.home");
   return (
     <div className="w-full h-10">
-      <h5 className="">{te("title")}</h5>
-      <h5 className="">{te("subtitle")}</h5>
+      <h5 className="">{any("title")}</h5>
+      <h5 className="">{any("description")}</h5>
     </div>
   );
 };
