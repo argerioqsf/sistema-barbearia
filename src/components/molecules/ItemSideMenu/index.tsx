a6efc1c906274a5fed90114935832647a51e2212
Argerio Queiroz <argerioaf3@gmail.com>
2024-02-06 10:08:11 -0300

    correcao

--- src/components/molecules/ItemSideMenu/index.tsx
+++ src/components/molecules/ItemSideMenu/index.tsx
@@ -7,6 +7,7 @@ import { ArrowRightIcon } from "@/components/Icons/ArrowRightIcon";
 import { IconSvgProps } from "@/types/general";
 import { twMerge } from "tailwind-merge";
 import { ArrowDownIcon } from "@/components/Icons/ArrowDownIcon";
+import { useRouter } from "next/navigation";
 
 type ItemSideMenuProps = {
   onClick?: (state: any) => void;
@@ -34,57 +35,61 @@ const ItemSideMenu: React.FC<ItemSideMenuProps> = ({
   bgcolorAvatar = "primary-100",
 }) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+
   function openSubMenu() {
     if (href) {
-      return;
+      return router.push(href);
     }
     setOpen(!open);
   }
   return (
     <div className="flex w-full flex-col min-h-[var(--navbar-height)] justify-start items-center border-b border-primary-50">
-      <Link className="w-full h-full" href={href} onClick={openSubMenu}>
-        <div className="w-full rounded-none h-full flex flex-row justify-between items-center px-6 py-4">
-          <div className="flex w-full flex-row justify-start items-center gap-4">
-            <Avatar
-              size={sizeAvatar}
-              icon={icon && icon}
-              image={image && image}
-              bgColor={bgcolorAvatar}
-            />
-            <Text className="text-lg font-bold text-white whitespace-nowrap overflow-hidden text-ellipsis">
-              {label}
-            </Text>
-          </div>
-
-          {href.length > 0 ||
-            (subMenuList &&
-              (open ? (
-                <ArrowRightIcon size={15} color="white" />
-              ) : (
-                <ArrowDownIcon size={20} color="white" />
-              )))}
+      <Button
+        className="w-full rounded-none h-full flex flex-row justify-between items-center px-6 py-4"
+        type="button"
+        onClick={openSubMenu}
+      >
+        <div className="flex w-full flex-row justify-start items-center gap-4">
+          <Avatar
+            size={sizeAvatar}
+            icon={icon && icon}
+            image={image && image}
+            bgColor={bgcolorAvatar}
+          />
+          <Text className="text-lg font-bold text-white whitespace-nowrap overflow-hidden text-ellipsis">
+            {label}
+          </Text>
         </div>
 
-        {subMenuList && (
-          <div
-            className={twMerge(
-              "w-full bg-primary-50 flex flex-col justify-between items-start  overflow-y-auto overflow-x-hidden whitespace-nowrap",
-              open === false && "hidden",
-              open === true && "flex"
-            )}
-          >
-            {subMenuList.map((menu) => (
-              <ItemSideMenu
-                sizeAvatar={15}
-                icon={menu.icon}
-                href={menu.href}
-                label={menu.label}
-                key={menu.id}
-              />
-            ))}
-          </div>
-        )}
-      </Link>
+        {href.length > 0 ||
+          (subMenuList &&
+            (open ? (
+              <ArrowRightIcon size={15} color="white" />
+            ) : (
+              <ArrowDownIcon size={20} color="white" />
+            )))}
+      </Button>
+
+      {subMenuList && (
+        <div
+          className={twMerge(
+            "w-full bg-primary-50 flex flex-col justify-between items-start  overflow-y-auto overflow-x-hidden whitespace-nowrap",
+            open === false && "hidden",
+            open === true && "flex"
+          )}
+        >
+          {subMenuList.map((menu) => (
+            <ItemSideMenu
+              sizeAvatar={15}
+              icon={menu.icon}
+              href={menu.href}
+              label={menu.label}
+              key={menu.id}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
