73a4ace63ad17f6b9127788946a98da0555e024f
Argerio Queiroz <argerioaf3@gmail.com>
2024-02-06 13:56:49 -0300

    adicionado logica de responsividade

--- src/components/template/SingIn/sections/SingInSection.tsx
+++ src/components/template/SingIn/sections/SingInSection.tsx
@@ -6,17 +6,19 @@ import { Text } from "@/components/atoms";
 const SingInSection = () => {
   return (
     <ContainerSection className="bg-primary-100">
-      <div className="w-full bg-primary-100 flex flex-row items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-        <div className="w-2/4 flex flex-row justify-end items-center pr-32">
+      <div className="w-full bg-primary-100 flex flex-col md:flex-row items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+        <div className="w-2/4 flex flex-col md:flex-row justify-center md:justify-end items-center pr-0 md:pr-[10vw]">
           {/* logo SIM */}
           <Avatar
             size={160}
             bgColor="black"
             image="https://www.grupomadretereza.com.br/sim/themes/simadm/assets/images/logo.png"
           />
-          <Text className="text-6xl font-bold text-white pl-6">SIM</Text>
+          <Text className="text-6xl font-bold text-white pl-0 pt-4 md:pt-0 md:pl-6">
+            SIM
+          </Text>
         </div>
-        <div className="w-2/4 flex flex-col justify-center items-start border-l-2  pl-32 h-[60vh]">
+        <div className="w-full md:w-2/4 flex flex-col justify-center items-start md:border-l-2 pl-0 md:pl-[10vw] h-auto md:h-[60vh]">
           <FormLogin />
         </div>
       </div>
