772510485b2ee0e9791f7aff7e9a04527fd9962d
Argerio Queiroz <argerioaf3@gmail.com>
2024-03-27 18:47:15 -0300

    ajuste

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/signin',
+        permanent: true,
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
