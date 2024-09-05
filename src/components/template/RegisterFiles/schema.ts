2ca06eb467719a5be33272f8ce9261f5f8f3ac3d
Argerio Queiroz <argerioaf3@gmail.com>
2024-09-04 23:37:42 -0300

    fix: ajuste

--- src/components/template/RegisterFiles/schema.ts
+++ src/components/template/RegisterFiles/schema.ts
@@ -1,3 +1,13 @@
 import { z } from 'zod'
 
-export const formSchemaRegisterFile = z.object({})
+export const formSchemaRegisterFile = z.object({
+  avatar: z
+    .instanceof(File)
+    .refine((file) => {
+      return file.type.match(/^image\/(jpg|jpeg|png|gif)$/)
+    }, 'Erro: O arquivo não é uma imagem válida.')
+    .refine((file) => {
+      const maxSize = 1024 * 1024 * 10
+      return file.size <= maxSize
+    }, 'Erro: O arquivo é muito grande.'),
+})
