8add36cd99cefc4f69e3875cfe37d1ec870b03e5
Argerio Queiroz <argerioaf3@gmail.com>
2024-09-05 00:27:59 -0300

    teste 2

--- src/components/template/RegisterFiles/schema.ts
+++ src/components/template/RegisterFiles/schema.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 
 const MAX_FILE_SIZE = 5000000
-const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png']
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+]
 
 export const formSchemaRegisterFile = z.object({
   avatar: z
